a0427be258dfad06b0d2df9d5691874d6b2f8ba7
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2024-09-24 11:38:51 +0200

    tak-schemas 4.0.0-RC1

--- src/main/java/se/skltp/tak/vagvalsinfo/wsdl/v2/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "urn:skl:tp:vagvalsinfo:v2", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.skltp.tak.vagvalsinfo.wsdl.v2;
